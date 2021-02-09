456ebf6b7978e55f45132623ff05b2f934d8d8b5
mirsaid-mirzohidov <mirzohidovm8@gmail.com>
2021-02-09 20:00:54 +0500

    Initial

--- apps/main/urls.py
+++ apps/main/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
+from .views import MainView
 
-from .views import index
 
 urlpatterns = [
-    path('', index),
+    path('', MainView.as_view(), name="index"),
 ]
--- apps/main/views.py
+++ apps/main/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
+from django.views.generic.base import View
+from .models import Course, Category
 
 
-def index(request):
-	return render(request, 
-		'main/index.html',
-		{
-		'title': 'DigitalEdu'
-		})
+class MainView(View):
+    """Основная страница"""
+    def get(self, request):
+
+    	# Филтруеть посты если пост не черновык то выведёт пост
+        query = Course.objects.filter(draft=False).order_by('-created_at')
+        categories = Category.objects.all()
+        
+
+        return render(request, 'main/index.html',
+    		{'nav_name': 'Основная страница',
+            'course_list': query,
+            'view': {'category': categories}})
